5aba73b746f05ab9a38064839fe4a31d79ad7ff1
Ju Dantas <jucienyds@gmail.com>
2022-04-26 10:20:01 -0300

    exemplo aula 02 sobre post

---
+++ aula-post-exemplo.js
@@ -0,0 +1,23 @@
+const express = require("express")
+const app = express()
+const port = 3000
+
+const produtos = []
+
+// configuração que permite uso do payload pelo body
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.get("/produto", (request, response) => {
+    return response.send(produtos)
+})
+
+app.post("/produto", (request, response) => {
+    const body = request.body
+    produtos.push(body)
+    return response.send(produtos)
+})
+
+app.listen(port, () => {
+    console.log("Está rodando aula 02 em:\n\nhttp://localhost:3000")
+})
